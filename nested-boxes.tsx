ab6d8354357469c126323fb6026aea3ce44b1c0b
damianprog <damianpukszta@gmail.com>
2026-09-07 11:34:12 +0200

    Typing props

---
+++ nested-boxes.tsx
@@ -0,0 +1,42 @@
+/**
+ * Things you could try:
+ *
+ * JSX.Element;
+ * JSX.Element | JSX.Element[];
+ * React.ReactNode;
+ * React.ReactChildren;
+ * React.ReactChild[];
+ */
+
+import type { PropsWithChildren } from "react";
+
+type BoxProps = { children: React.ReactNode }; /* 👈 Get rid of this! 👆 */
+
+const Box = ({ children }: PropsWithChildren) => {
+  return (
+    <section
+      className="m-4"
+      style={{ padding: "1em", border: "5px solid purple" }}
+    >
+      {children}
+    </section>
+  );
+};
+
+export const NestedBoxes = () => {
+  return (
+    <main className="m-8">
+      <Box>
+        Just a string.
+        <p>Some HTML that is not nested.</p>
+        <Box>
+          <h2>Another React component with one child.</h2>
+        </Box>
+        <Box>
+          <h2 className="mb-4">A nested React component with two children.</h2>
+          <p>The second child.</p>
+        </Box>
+      </Box>
+    </main>
+  );
+};
