2c175f3d6a7ee82598eaa2ec2e612749b0ce8c8a
damianprog <damianpukszta@gmail.com>
2026-09-14 16:01:02 +0200

    Typing async data with zod

---
+++ daily-closure/news-article.tsx
@@ -0,0 +1,47 @@
+import { Card } from "$/common/components/card";
+import { useEffect, useState } from "react";
+import { currentDate } from "./utilities";
+import type { Post } from "./types";
+import z from "zod";
+
+type NewsArticleProps = {
+  id: number;
+};
+
+const PostSchema = z.object({
+  id: z.number(),
+  title: z.string(),
+  body: z.string(),
+}) satisfies z.ZodType<Post>;
+
+// If you're trying to make schemas that validate the type,
+// satisfies utility type will make sure that they match each other
+
+// instead of importing Post type
+// type Post = z.infer<typeof PostSchema>;
+
+const fetchArticle = async (id: number): Promise<Post> => {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  const possiblePost = response.json();
+  return PostSchema.parse(possiblePost);
+};
+
+export const NewsArticle = ({ id = 1 }: NewsArticleProps) => {
+  // Important: The type for article is any because the API returns.
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [article, setArticle] = useState<any>(null);
+
+  useEffect(() => {
+    fetchArticle(id).then((data) => setArticle(data));
+  }, [id]);
+
+  return (
+    <Card as="article" className="space-y-4 font-mono md:first:col-span-2">
+      <header className="flex items-start justify-between">
+        <h2 className="text-lg font-semibold">{article?.title}</h2>
+        <p className="text-sm whitespace-nowrap text-gray-500">{currentDate}</p>
+      </header>
+      <p>{article?.body}</p>
+    </Card>
+  );
+};
