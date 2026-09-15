152101d10db79c47eedf1aaacd464a87716f49cd
damianprog <damianpukszta@gmail.com>
2026-09-15 10:48:40 +0200

    Advanced zod types and use() hook and suspense types

--- daily-closure/news-article.tsx
+++ daily-closure/news-article.tsx
@@ -1,7 +1,7 @@
 import { Card } from "$/common/components/card";
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import { currentDate } from "./utilities";
-import type { Post } from "./types";
+// import type { Post } from "./types";
 import z from "zod";
 
 type NewsArticleProps = {
@@ -9,10 +9,18 @@ type NewsArticleProps = {
 };
 
 const PostSchema = z.object({
-  id: z.number(),
+  id: z.coerce.number(),
   title: z.string(),
   body: z.string(),
-}) satisfies z.ZodType<Post>;
+  // authorEmail: z.email(),
+  // published: z.coerce.boolean(),
+  // tags: z.array(z.string()).default([]),
+  // metadata: z.record(z.string(), z.unknown()),
+});
+
+// const IdSchema = z.union([z.string(), z.number(), z.null(), z.boolean()]);
+
+type Post = z.infer<typeof PostSchema>;
 
 // If you're trying to make schemas that validate the type,
 // satisfies utility type will make sure that they match each other
@@ -21,19 +29,23 @@ const PostSchema = z.object({
 // type Post = z.infer<typeof PostSchema>;
 
 const fetchArticle = async (id: number): Promise<Post> => {
-  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${id}`,
+  );
   const possiblePost = response.json();
   return PostSchema.parse(possiblePost);
 };
 
 export const NewsArticle = ({ id = 1 }: NewsArticleProps) => {
   // Important: The type for article is any because the API returns.
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const [article, setArticle] = useState<any>(null);
 
-  useEffect(() => {
-    fetchArticle(id).then((data) => setArticle(data));
-  }, [id]);
+  // const [article, setArticle] = useState<any>(null);
+
+  // useEffect(() => {
+  //   fetchArticle(id).then((data) => setArticle(data));
+  // }, [id]);
+
+  const article = use(fetchArticle(id));
 
   return (
     <Card as="article" className="space-y-4 font-mono md:first:col-span-2">
